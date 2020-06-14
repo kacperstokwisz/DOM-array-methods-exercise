40ce5012e68d88912e268fb738493b01d7f0c21a
Kacper Stokwisz <kacperstokwisz@gmail.com>
2020-06-14 22:14:13 +0200

    Add calculateWealth function

--- script.js
+++ script.js
@@ -41,10 +41,24 @@ function sortByRichest() {
     updateDOM();
 }
 
+// Filter only milionaires
+function showMillionaires() {
+    data = data.filter(user => user.money > 1000000);
+    updateDOM(); 
+}
+
 // Add new object to data array
 function addData(obj){
     data.push(obj);
     updateDOM();
+}
+
+// Calculate Wealth
+function calculateWealth() {
+    const wealth = data.reduce((acc, user) => acc += user.money, 0);
+    const wealthElement = document.createElement('div');
+    wealthElement.innerHTML = `<h3>Total Wealth: <strong>${formatMoney(wealth)}</strong></h3>`;
+    main.appendChild(wealthElement)
 }
 
 // Update DOM
@@ -67,3 +81,5 @@ function formatMoney(number){
 addUserBtn.addEventListener('click', getRandomUser);
 doubleBtn.addEventListener('click', doubleMoney);
 sortBtn.addEventListener('click', sortByRichest);
+showMillionairesBtn.addEventListener('click', showMillionaires);
+calculateWealthBtn.addEventListener('click', calculateWealth);
